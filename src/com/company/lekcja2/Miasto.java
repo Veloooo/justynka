4bc84a4fb6c8d18efb31ed103c19b23b919a403c
Lukease <lukpawlowski1996@gmail.com>
2021-08-19 15:09:03 +0200

    Dodalem Miasto

---
+++ src/com/company/lekcja2/Miasto.java
@@ -0,0 +1,15 @@
+package com.company.lekcja2;
+
+import java.awt.*;
+
+public class Miasto {
+    private int liczbaLudnosci;
+    private String nazwa;
+    private Point polozenie;
+
+    public Miasto(int liczbaLudnosci, String nazwa, Point polozenie) {
+        this.liczbaLudnosci = liczbaLudnosci;
+        this.nazwa = nazwa;
+        this.polozenie = polozenie;
+    }
+}
